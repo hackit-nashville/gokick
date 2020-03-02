2d5b0be9ed66fe90bb59ebbe4edfce2d49ede811
Miles Maddox <j.milesmaddox@gmail.com>
2020-03-02 10:20:37 -0600

    init

--- cmd/hello.go
+++ cmd/hello.go
@@ -1,20 +1,16 @@
 package cmd
 
 import (
-	lib "go-cli-starter-template/lib"
+	lib "github.com/hackit-nashville/gokick/lib"
 
 	"github.com/spf13/cobra"
 )
 
 // Run Command ./pentaho-cli run
-var helloCmd = &cobra.Command{
+var HelloCmd = &cobra.Command{
 	Use:   "hello",
 	Short: "Say hello",
 	Run: func(cmd *cobra.Command, args []string) {
 		lib.HelloWorld()
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(helloCmd)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module go-cli-starter-template
+module github.com/hackit-nashville/gokick
 
 go 1.12
 
-require github.com/spf13/cobra v0.0.5
+require github.com/spf13/cobra v0.0.6
